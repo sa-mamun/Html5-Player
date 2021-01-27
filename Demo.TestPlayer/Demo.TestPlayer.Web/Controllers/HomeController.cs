4a794f247e7cb428af3aefdda195d62becf6da1d
sa-mamun <samamun009@gmail.com>
2021-01-27 18:26:41 +0600

    Multiple Player Added

--- Demo.TestPlayer/Demo.TestPlayer.Web/Controllers/HomeController.cs
+++ Demo.TestPlayer/Demo.TestPlayer.Web/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Demo.TestPlayer.Web.Common;
+using Demo.TestPlayer.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -21,7 +24,15 @@ namespace Demo.TestPlayer.Web.Controllers
 
         public ActionResult Resolution()
         {
-            return View();
+            var list = new List<VideoModel>()
+            {
+                new VideoModel { Poster = "", Url = "https://upload.wikimedia.org/wikipedia/commons/transcoded/a/ab/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm.720p.webm", Type="video/webm" },
+                new VideoModel { Poster = "", Url = "https://upload.wikimedia.org/wikipedia/commons/transcoded/a/ab/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm.720p.webm", Type="video/webm" },
+                new VideoModel { Poster = "", Url = "https://upload.wikimedia.org/wikipedia/commons/transcoded/a/ab/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm.720p.webm", Type="video/webm" },
+                new VideoModel { Poster = "", Url = "https://upload.wikimedia.org/wikipedia/commons/transcoded/a/ab/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm/Caminandes_3_-_Llamigos_-_Blender_Animated_Short.webm.720p.webm", Type="video/webm" }
+            };
+            
+            return View(list);
         }
 
         public ActionResult Bitrate()
@@ -33,6 +44,37 @@ namespace Demo.TestPlayer.Web.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //public ActionResult HlsTests()
+        //{
+        //    ProcessStartInfo startInfo = new ProcessStartInfo();
+        //    startInfo.CreateNoWindow = false;
+        //    startInfo.UseShellExecute = false;
+        //    startInfo.FileName = Path.Combine(@"C:\ffmpeg", "ffmpeg.exe");
+
+        //    //????????
+
+        //    startInfo.Arguments = "ffmpeg -i http://192.168.2.181:8084/Videos/indexes/index.m3u8 -c copy -bsf:a aac_adtstoasc C://earth.mp4";
+
+        //    //????????
+
+        //    startInfo.RedirectStandardOutput = true;
+
+        //    try
+        //    {
+        //        using (Process process = Process.Start(startInfo))
+        //        {
+        //            process.WaitForExit();
+        //        }
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw;
+        //    }
+        //    return RedirectToAction("HlsTest");
+        //}
 
         [HttpGet]
         public ActionResult GetSource()
---
+++ Demo.TestPlayer/Demo.TestPlayer.Web/Models/VideoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.TestPlayer.Web.Models
+{
+    public class VideoModel
+    {
+        public string Poster { get; set; }
+        public string Url { get; set; }
+        public string Type { get; set; }
+    }
+}
